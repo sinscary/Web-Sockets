2102625e94957bef7542fe3e167935c5db3f1abe
sinscary <nbsharma@outlook.com>
2016-10-08 19:58:14 +0530

    Add socket io on cient and server

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ var express = require('express');
 var app = express();
 
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function(){
+	console.log('User connected via socket.io');
+}); //it lets to listen for events
 
 http.listen(PORT, function(){
 	console.log('Server started');
